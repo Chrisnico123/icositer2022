1d923483776910e2bfc86f3e5e8a0e35c91f756e
riecho14 <riecho14@outlook.com>
2022-08-05 13:00:07 +0700

    fix page1

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -18,35 +18,34 @@
 
 <body>
     <!-- Navbar -->
-    <header class="header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="{{ asset('assets') }}/images/logo.png" width="77.2" height="58.5" alt="">
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#home">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#speakers">Speakers</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#info">Info</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About</a>
-                        </li>
-                    </ul>
-                    <a href="#register" type="button" class="btn btn-danger">Register</a>
-                </div>
-        </nav>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets') }}/images/logo.png" width="77.2" height="58.5" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#speakers">Speakers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#info">Info</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                </ul>
+                <a href="#register" type="button" class="btn btn-danger">Register</a>
+            </div>
+        </div>
+    </nav>
 
     <!-- Home -->
     <section>
@@ -54,7 +53,6 @@
         <img src="{{ asset('assets') }}/images/bigcloud.svg" id="bigcloud">
         <img src="{{ asset('assets') }}/images/mountain.svg" id="mountain">
         <img src="{{ asset('assets') }}/images/airballoon.svg" id="airballoon">
-        <img src="{{ asset('assets') }}/images/airballoon2.svg" id="airballoon2">
         <img src="{{ asset('assets') }}/images/pole.svg" id="pole">
         <img src="{{ asset('assets') }}/images/prop.svg" id="prop">
         <img src="{{ asset('assets') }}/images/prop2.svg" id="prop2">
