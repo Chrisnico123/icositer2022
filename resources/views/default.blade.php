ed456f69637952257718d67f6d14792644aac4cb
riecho14 <riecho14@outlook.com>
2022-08-05 13:45:21 +0700

    add section page

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -49,6 +49,7 @@
 
     <!-- Home -->
     <section>
+        <h2 id="text">ICoSITeR<br>2022</h2>
         <img src="{{ asset('assets') }}/images/clouds.svg" id="clouds">
         <img src="{{ asset('assets') }}/images/bigcloud.svg" id="bigcloud">
         <img src="{{ asset('assets') }}/images/mountain.svg" id="mountain">
@@ -58,6 +59,54 @@
         <img src="{{ asset('assets') }}/images/prop2.svg" id="prop2">
         <img src="{{ asset('assets') }}/images/ground.svg" id="ground">
     </section>
+
+    <div class="page2">
+        <h2>NEWS</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada porttitor vestibulum. Sed suscipit
+            sollicitudin dui non porta. Curabitur hendrerit turpis leo, a sagittis tellus consectetur in. Nam vehicula
+            eleifend sapien dictum ornare. Integer vehicula id ligula ac venenatis. Mauris non gravida erat. Phasellus
+            sed augue tortor. Mauris vestibulum arcu libero. Nunc placerat, nisl vitae pellentesque gravida, est neque
+            hendrerit quam, sit amet molestie lacus risus eu diam. Maecenas sollicitudin tincidunt consectetur. Vivamus
+            fermentum mi in turpis commodo, in fermentum sapien faucibus. Nam auctor non massa vitae condimentum. In
+            tortor quam, volutpat ac lacus sed, imperdiet ultrices risus. Cras sed mollis est.
+
+            In quis accumsan neque, vitae auctor neque. Nunc luctus volutpat interdum. Maecenas dapibus, massa eu
+            sodales sodales, augue tellus varius magna, in posuere odio ligula ac lorem. Integer euismod quam a neque
+            pharetra, in tincidunt dui posuere. Pellentesque venenatis libero et felis ultrices mattis. Nam mattis velit
+            non diam ultricies sollicitudin. Aenean facilisis lorem augue, ut aliquet nisl posuere at. Donec egestas
+            risus eu lacus volutpat, malesuada dapibus libero dapibus. Fusce euismod mi eget diam convallis egestas.
+            Nunc quam dui, sollicitudin id dignissim quis, molestie porta lorem. Integer vulputate sem nibh, sed sodales
+            odio ultrices sit amet. Mauris fermentum, enim vel tempor vulputate, massa sem eleifend nisi, mattis
+            placerat justo quam id arcu. Donec ac venenatis orci, sit amet euismod neque. Sed rutrum neque in leo
+            accumsan, ut placerat justo sodales. Nullam finibus odio non massa rhoncus sagittis non non nunc.
+
+            Aenean gravida luctus erat eu sagittis. Maecenas suscipit congue interdum. Quisque aliquam, felis commodo
+            maximus pellentesque, libero neque gravida orci, nec pellentesque elit dolor eget lorem. Fusce iaculis eget
+            nulla ac pellentesque. Proin eu luctus turpis, non molestie est. Quisque bibendum feugiat nunc, vitae
+            facilisis est placerat at. Suspendisse ut leo laoreet, semper risus sed, ultrices felis. Duis vitae aliquam
+            justo, nec iaculis dolor. Maecenas interdum lacus vitae venenatis varius. Suspendisse eleifend quam sit amet
+            pellentesque convallis. Sed id rhoncus libero. Vivamus tincidunt tempor varius. Proin felis ligula, lobortis
+            sed aliquam eu, posuere eu sapien. Quisque facilisis, augue nec fermentum iaculis, diam leo fermentum velit,
+            ac vestibulum ex quam nec nunc. Morbi luctus vestibulum ligula et venenatis. Suspendisse non risus varius,
+            vehicula quam ut, venenatis risus.
+
+            Praesent dignissim eget mi aliquam rhoncus. Suspendisse augue turpis, suscipit non egestas sed, mollis
+            mollis odio. Sed felis ligula, varius eget augue non, pellentesque gravida lorem. Quisque pharetra, sapien
+            ut tincidunt pellentesque, lectus tellus luctus ipsum, et congue urna ligula vitae risus. Fusce ultrices ut
+            magna ac fermentum. Curabitur consectetur tortor sed risus vestibulum sollicitudin. Maecenas consequat dolor
+            at tempor euismod. Sed in eros at nisi rhoncus consequat. Maecenas orci orci, condimentum sed neque ut,
+            maximus ultricies tortor. Donec lectus augue, vestibulum vitae ipsum malesuada, sodales tempor diam. Ut
+            maximus tortor magna, ut viverra nunc bibendum non. Aliquam aliquet massa at sapien dictum aliquet commodo
+            at tortor. Duis id leo dapibus, rhoncus arcu vitae, elementum quam.
+
+            Maecenas eu tellus dolor. Donec pharetra efficitur nisi, nec egestas risus. Mauris vel euismod tortor, ut
+            dictum arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Nulla in iaculis mi, eget commodo sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+            Vestibulum sed venenatis felis. Aliquam rutrum mi ac enim sodales lobortis. Integer laoreet lobortis purus
+            in tristique. Cras nisi dui, egestas nec lectus sed, dignissim ultricies neque. Vivamus nibh eros, auctor ut
+            nunc sit amet, dapibus dapibus arcu. Vivamus tristique est sed imperdiet ullamcorper. Phasellus vehicula
+            eget mauris ac vulputate. Maecenas rutrum dui lacus, a sollicitudin risus accumsan ut. In ac ante augue.</p>
+    </div>
 </body>
 
 </html>
