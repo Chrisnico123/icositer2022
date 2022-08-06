0235b0bab044a500ddfdc6ffbf1f6cba2b5a8f60
riecho14 <riecho14@outlook.com>
2022-08-06 12:18:00 +0700

    text parallax scroll effect

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -23,7 +23,8 @@
             <a class="navbar-brand" href="#">
                 <img src="{{ asset('assets') }}/images/logo.png" width="77.2" height="58.5" alt="">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -59,6 +60,22 @@
         <img src="{{ asset('assets') }}/images/ground.svg" id="ground">
     </section>
 
+    <script>
+        let text = document.getElementById('text');
+        let clouds = document.getElementById('clouds');
+        let bigcloud = document.getElementById('bigcloud');
+        let mountain = document.getElementById('mountain');
+        let airballoon = document.getElementById('airballoon');
+        let pole = document.getElementById('pole');
+        let prop = document.getElementById('prop');
+        let prop2 = document.getElementById('prop2');
+        let ground = document.getElementById('ground');
+
+        window.addEventListener('scroll', function() {
+            let value = window.scrollY;
+            text.style.top = 149 + value * 0.5 + 'px';
+        });
+    </script>
     <!-- Page2 -->
 
     <div class="page2">
@@ -66,12 +83,18 @@
             <div class="row">
                 <div class="col">
                     <h2>News</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada porttitor vestibulum. Sed suscipit
-                        sollicitudin dui non porta. Curabitur hendrerit turpis leo, a sagittis tellus consectetur in. Nam vehicula
-                        eleifend sapien dictum ornare. Integer vehicula id ligula ac venenatis. Mauris non gravida erat. Phasellus
-                        sed augue tortor. Mauris vestibulum arcu libero. Nunc placerat, nisl vitae pellentesque gravida, est neque
-                        hendrerit quam, sit amet molestie lacus risus eu diam. Maecenas sollicitudin tincidunt consectetur. Vivamus
-                        fermentum mi in turpis commodo, in fermentum sapien faucibus. Nam auctor non massa vitae condimentum. In
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada porttitor vestibulum. Sed
+                        suscipit
+                        sollicitudin dui non porta. Curabitur hendrerit turpis leo, a sagittis tellus consectetur in.
+                        Nam vehicula
+                        eleifend sapien dictum ornare. Integer vehicula id ligula ac venenatis. Mauris non gravida erat.
+                        Phasellus
+                        sed augue tortor. Mauris vestibulum arcu libero. Nunc placerat, nisl vitae pellentesque gravida,
+                        est neque
+                        hendrerit quam, sit amet molestie lacus risus eu diam. Maecenas sollicitudin tincidunt
+                        consectetur. Vivamus
+                        fermentum mi in turpis commodo, in fermentum sapien faucibus. Nam auctor non massa vitae
+                        condimentum. In
                         tortor quam, volutpat ac lacus sed, imperdiet ultrices risus. Cras sed mollis est.
 
                     <div class="button">
@@ -80,7 +103,8 @@
                 </div>
 
                 <div class="col">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                        class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
                 </div>
 
             </div>
@@ -95,27 +119,35 @@
         <h2>GALLERY</H2>
         <div class="row">
             <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" />
             </div>
 
             <div class="col-lg-3 mb-4 mb-lg-0">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp" class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
             </div>
 
             <div class="col-lg-3 mb-4 mb-lg-0">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp" class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp" class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
             </div>
 
             <div class="col-lg-3 mb-4 mb-lg-0">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp" class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
             </div>
         </div>
     </div>
