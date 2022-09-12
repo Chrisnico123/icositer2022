e9ea937ee05c22b566ad92f8f6d490e4ce4e5671
Bayu Nindioko <nbayun2@gmail.com>
2022-09-12 17:05:06 +0700

    page divisi

---
+++ resources/views/divisi.blade.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Title -->
+    <title>ICoSITeR 2022</title>
+
+    <!-- Styling -->
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/style.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="{{ asset('assets/imagehover.css/css/imagehover.min.css') }}" />
+
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container ">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('assets') }}/images/logo.png" width="77.2" height="58.5" alt="">
+            </a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#speakers">Speakers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                </ul>
+                <a href="/register" type="button" class="btn btn-danger">Register</a>
+            </div>
+        </div>
+    </nav>
+    <div class="organigram">
+        <h2 class="title-organigram"> Ketua Divisi </h2>
+        <h2 class="title-organigram-1">ICoSITeR</h2>
+
+        <div class="container text-center">
+            <div class="row row-cols-2">
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+                
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+
+                <div class="col-organigram">
+                    <img src="{{ asset('assets') }}/images/avatar-organigram.png" height="350px" alt="">
+                    <h2 class="nama">Rahman Pajri</h2>
+                    <p class="jabatan">Ketua Divisi xxxxx <br> TSE'20</br></p>
+                    <div class="text-center">
+                        <button type="button" class="btn btn-success">Lihat Selengkapnya</button>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+    </div>
+    <!-- Footer -->
+    <footer>
+        <div class="atas">
+            <div class="icos">
+                <img src="{{ asset('assets') }}/images/logo.png" width="77.2" height="58.5" alt="">
+            </div>
+            <div class="alamat">
+                <p>Jl.Terusan Ryacudu,Wayhuwi,Kec,Jati Agung,
+                    <br>
+                    Kabupaten Lampung Selatan,Lampung,35365
+                </p>
+            </div>
+        </div>
+        <div class="bawah">
+            <div class="email">
+                <div class="vektor1">
+                    <img src="{{ asset('assets') }}/images/vektor1.png" width="" height="" alt="">
+                </div>
+                <div class="par1">
+                    <p>icositer2022@gmail.com</p>
+                </div>
+            </div>
+            <div class="telepon">
+                <div class="vektor2">
+                    <img src="{{ asset('assets') }}/images/Vector2.png" width="" height="" alt="">
+                </div>
+                <div class="par2">
+                    <p>+6281282434826</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
